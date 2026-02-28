6a3fc287d81ba77ef91f32acd9ce6c8f68734d8f
Aryan Lodha <aryanlodha@Aryans-MacBook-Air.local>
2026-02-28 12:34:06 +0000

    CORS Fix

--- app/main.py
+++ app/main.py
@@ -32,11 +32,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://environmental-risk-platform.vercel.app"
-    ],
+    allow_origins=["*"],   # 🔥 Allow all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
